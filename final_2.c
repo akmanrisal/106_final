f5a581b9ee7a283b5d80289eff70e3a8ad5162a6
akmanrisal <akmanrisal.r@ku.th>
2021-03-29 18:00:10 +0700

    test

--- final_2.c
+++ final_2.c
@@ -3,29 +3,44 @@ int main()
 {
     int n;
     scanf("%d",&n);
-    int num[n],max,end,sum=1;
-    for(int i=0;i<n;i++)
+    int num[n],max,end,sum=1,a=0;
+    if(n>=2 && n<=1000)
     {
-        scanf("%d",&num[i]);
-    }
-    max=num[0];
-    for(int i=1;i<n;i++)
-    {
-        if(max<num[i])
+        for(int i=0; i<n; i++)
         {
-            max=num[i];
-            end=num[i];
+            scanf("%d",&num[i]);
         }
-    }
-    for(int i=0;i<n;i++)
-    {
-        if(end!=num[i])
+        max=num[0];
+        end=num[0];
+        for(int i=1; i<n; i++)
         {
-             sum++;
+            if(max<=num[i])
+            {
+                if(max==num[i])
+                 {
+                     a++;
+                 }
+
+                max=num[i];
+                end=num[i];
+            }
+
         }
+        for(int i=0; i<n; i++)
+        {
+            if(end!=num[i])
+                sum++;
+            else
+                break;
+        }
+        if(a==0)
+            printf("%d",sum);
         else
-            break;
+            printf("%d %d",sum,max);
     }
-    printf("%d",sum);
+    else
+        printf("Error");
+
+    return 0;
 
 }
