8e72cacce950395a359b5ae2afa47302c341b936
akmanrisal <akmanrisal.r@ku.th>
2021-03-29 17:36:52 +0700

    test

--- final_2.c
+++ final_2.c
@@ -3,7 +3,7 @@ int main()
 {
     int n;
     scanf("%d",&n);
-    int num[n],max,end;
+    int num[n],max,end,sum=1;
     for(int i=0;i<n;i++)
     {
         scanf("%d",&num[i]);
@@ -14,9 +14,18 @@ int main()
         if(max<num[i])
         {
             max=num[i];
-            //end=max;
+            end=num[i];
         }
     }
-    printf("%d",max);
+    for(int i=0;i<n;i++)
+    {
+        if(end!=num[i])
+        {
+             sum++;
+        }
+        else
+            break;
+    }
+    printf("%d",sum);
 
 }
