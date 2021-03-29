3780c9c7ebc6e27e772105001f2c78e707a9c2fe
akmanrisal <akmanrisal.r@ku.th>
2021-03-29 19:38:20 +0700

    test

--- final_1.c
+++ final_1.c
@@ -1,15 +1,28 @@
 #include<stdio.h>
 int main()
 {
-    int n,k,c;
+    int n,k,c,sum;
     scanf("%d %d",&n,&c);
-
-    for(int i=n;; n++)
+    sum=n;
+    if(sum>0 || sum<32750)
     {
-        if(n%10==c)
+        for(int i=sum;;sum++)
         {
-            printf("%d",n);
-            break;
+            if(sum%10==c)
+            {
+                k=sum;
+                if(k>n)
+                {
+                    printf("%d",k);
+                    break;
+                }
+                else
+                    continue;
+            }
         }
     }
+    else
+        printf("Error");
+
+    return 0;
 }
